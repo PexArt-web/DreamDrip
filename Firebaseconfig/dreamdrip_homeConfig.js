d9dc95fb7041d418a0816ad451aa48fcadfe5504
PexArt-web <pexart74@gmail.com>
2024-05-10 16:06:03 +0800

    new chnages

--- Firebaseconfig/dreamdrip_homeConfig.js
+++ Firebaseconfig/dreamdrip_homeConfig.js
@@ -1,7 +1,21 @@
+const clip = document.querySelector('.bi-paperclip')
+const fileTrig = document.querySelector('.files')
 
+clip.addEventListener('click',()=>{
+    fileTrig.click()
+})
 
-const textPost = document.querySelector('.textPost')
+const fileValue = document.querySelector('.fileValue')
 
-textPost.addEventListener('input',()=>{
-    console.log(textPost.value);
+fileTrig.addEventListener('change',(input)=>{
+    console.log(fileTrig.value);
+    if (input.files && input.files[0]) {
+        const fileReader = new FileReader()
+
+        fileReader.onload=(e)=>{
+            `<img src = ${e.target.result}>`
+
+            fileReader.readAsDataURL(input.files[0])
+        }
+    }
 })
