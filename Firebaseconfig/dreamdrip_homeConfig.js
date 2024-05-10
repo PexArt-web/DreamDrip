854d42f0eb506a3089a5199fcc547618ee7ac978
PexArt-web <pexart74@gmail.com>
2024-05-10 18:31:46 +0800

    this new

--- Firebaseconfig/dreamdrip_homeConfig.js
+++ Firebaseconfig/dreamdrip_homeConfig.js
@@ -8,14 +8,8 @@ clip.addEventListener('click',()=>{
 const fileValue = document.querySelector('.fileValue')
 
 fileTrig.addEventListener('change',(input)=>{
-    console.log(fileTrig.value);
-    if (input.files && input.files[0]) {
-        const fileReader = new FileReader()
-
-        fileReader.onload=(e)=>{
-            `<img src = ${e.target.result}>`
-
-            fileReader.readAsDataURL(input.files[0])
-        }
+    const [file] = fileTrig.files
+    if (file) {
+        fileValue.src = URL.createObjectURL(file)
     }
 })
